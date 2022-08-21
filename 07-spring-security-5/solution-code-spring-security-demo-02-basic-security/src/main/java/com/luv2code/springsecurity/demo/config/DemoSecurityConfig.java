d8a2968ffecaa6e7af1c43f6cb69268203081eb5
Chad Darby <darby@luv2code.com>
2022-08-21 09:48:42 -0400

    Updated to Spring Security 5.7.3 - resolved deprecated code

--- 07-spring-security-5/solution-code-spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ 07-spring-security-5/solution-code-spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,30 +1,42 @@
 package com.luv2code.springsecurity.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DemoSecurityConfig {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+    	
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
 
-		// add our users for in memory authentication
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("ADMIN"));
-	}
-	
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(john, mary, susan);
+        
+    }
+
+    
 }
 
 
